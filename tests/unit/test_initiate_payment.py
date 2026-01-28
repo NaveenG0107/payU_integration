afd0a61f0d8fdd9ebb2c42051d808538afed264c
unknown <naveenkumar.g@mitrahsoft.in>
2026-01-28 12:34:11 +0530

    add test case file

---
+++ tests/unit/test_initiate_payment.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_pay_productinfo():
+
+    response = client.post("https://lankly-multipointed-kirstie.ngrok-free.dev/api/pay")
+
+    # API should respond
+    assert response.status_code == 200
+
+    html = response.text
+
+    # Check productinfo in HTML
+    assert 'name="productinfo" value="Test Product"' in html
