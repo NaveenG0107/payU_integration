a2baec8374f27c19ec336f47e06f8d8a3f6ad22b
unknown <naveenkumar.g@mitrahsoft.in>
2026-01-28 20:22:08 +0530

    testing purpose

--- tests/unit/test_initiate_payment.py
+++ tests/unit/test_initiate_payment.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 
 def test_pay_productinfo():
 
-    response = client.post("https://lankly-multipointed-kirstie.ngrok-free.dev/api/payy")
+    response = client.post("https://lankly-multipointed-kirstie.ngrok-free.dev/api/pay")
 
     # API should respond
     assert response.status_code == 200
